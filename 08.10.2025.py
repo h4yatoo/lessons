57083a6c30d82a973c607d24c3c6335669e448ef
h4yatoo <balandin27072008@gmail.com>
2025-10-08 18:00:15 +0200

    first commit

--- 08.10.2025.py
+++ 08.10.2025.py
@@ -1,1 +1,92 @@
 #это первый созданный комментарий asdds
+
+#int - целочисленный тип данных
+# a=10
+# print(type(a)) # type(obj) - выводит тип данных объекта
+# float - число с точкой
+# a=10.23
+# print(type(a))
+#str - строковый тип данных
+# a='awsd'
+# print(type(a))
+#bool - логический тип 0/1 или True/False
+# a=True
+# print(type(a))
+#list - список
+# a=[1,2,3]
+# print(type(a))
+
+#set - набор уникальных значений (в фигурных скобочках{})
+# a={1,2,3,4,5,6,6}
+# print(type(a),a)
+#tuple - кортеж (набор неизменяемых данных)
+# a=(1,2,3,4,5)
+# print(type(a),a)
+#dict - словарь с элементами ключь: значение
+# a={'a':1234}
+# print(type(a))
+
+# a=10
+# print(a,a,a,a,sep='')
+#sep - разделитель при выводе
+
+
+#a=input()#input - возвращает введенную в консоли str строку
+#print(a)
+
+#a=int(input())#перевод из str v int
+#print(a+1)
+
+
+#a=list('hello world')
+#print(a)
+
+#a=int(float(input()))
+#print(a+1)
+
+#a='\tPrivet\ndrug'#\t - отступ на 4 пробела или 1 таб \n - переход на новую строку
+#print(a)
+
+#все элементы начинаются с 0
+# a=[1,2,3]
+# b='abcd'
+# c=(1,2,3)
+# print(c[0])
+# c=list(c)
+# c[0]=10
+# print(c[0])
+
+# Задача1
+# a={'Имя':'Иван', 'Фамилия':'Иванов', 'Возраст':17, 'Класс':'11A'}
+# print(a)
+
+
+# Задача2
+# a=45
+# b=str(a)
+# print('Число в строке:',b)
+
+#Задача 3
+# a='asd'
+# b='wqwe'
+# c='ghj'
+# print(a,b,c,sep=',')
+
+
+#Задача 4
+# a='\tПривет, \nFuture step'
+# print(a)
+
+
+#Задача 5
+# a,b=int(float(input())),int(float(input()))
+# print(a+b)
+
+
+# a=input()
+# b=a.split(' ')#делит строку по разделителю, возварщает list
+# c=int(float(b[0]))
+# d=int(float(b[1]))
+# print(c+d)
+
+
