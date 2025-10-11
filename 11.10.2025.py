dcb0d88108f7b53e99163287787baaf0bdd4feac
h4yatoo <balandin27072008@gmail.com>
2025-10-11 19:38:13 +0200

    sss

---
+++ 11.10.2025.py
@@ -0,0 +1,103 @@
+# a=int(input())
+# if a%4==0 and a%100!=0 or a%400==0:
+#     print("YES")
+# else:
+#     print('NO')
+
+
+# a=int(input())
+# p=3.14
+# if a==1:
+#     a1=int(input())
+#     print(float(a1*2*p))
+# elif a==2:
+#     b1,b2=map(int,input().split())
+#     print(float(b1*b2*p))
+# elif a!=1 and a!=2:
+#     print('ERROR')
+
+# a=int(input())
+# if ([0]%2==0 or [0]%5==0 and [1]%2==0 or [1]%5==0) or ([1]%2==0 or [1]%5==0 and [2]%2==0 or a[2]%5==0) or ([0]%2==0 or [0]%5==0 and [2]%2==0 or [2]%5==0):
+#     print(a)
+# else:
+#     print(a+1)
+
+# a=int(input())
+# count=0
+# a1=a%10
+# a2=a//100
+# a3=a//10%10
+# if a1%2==0 or a1%5==0:
+#     count+=1
+# if a2%2==0 or a2%5==0:
+#     count+=1
+# if a3%2==0 or a3%5==0:
+#     count+=1
+# if count>=2:
+#     print(a)
+# else:
+#     print(a+1)
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# if a+b>c and a+c>b and b+c>a:
+#     if a==b==c:
+#         print('равностор')
+#     elif a==b or a==c or b==c:
+#         print('равнобедр')
+#     else:
+#         print('разностор')
+# else:
+#     print('такого нет')
+
+
+# a=int(input())
+# match a:
+#     case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+#         print('31')
+#     case 4 | 6 | 9 | 11:
+#         print('30')
+#     case 2:
+#         print('28')
+#     case _:
+#         print('такого нет')
+
+
+# a=input()
+# b=int(input())
+# match a:
+#     case 'Рабочий':
+#         if 9<=b<=18:
+#             print('Рабочее время')
+#     case 'Выходной':
+#         if 10<=b<=16:
+#             print('Рабочее время')
+#     case 'Праздничный':
+#         print('Не работает')
+#     case _:
+#         print('ERROR')
+
+
+# a=input('+,-,*,/')
+# b=int(input())
+# c=int(input())
+# match a:
+#     case '+':
+#         print(b+c)
+#     case '-':
+#         print(b-c)
+#     case '*':
+#         print(b*c)
+#     case '/':
+#         print(b/c)
+
+
+
+
+
+
+
+
+
+
