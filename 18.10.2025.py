9d16a5da002c7987bea46f4a9e13f8cb14fb9fb2
h4yatoo <balandin27072008@gmail.com>
2025-10-18 19:36:04 +0200

    clwrk

---
+++ 18.10.2025.py
@@ -0,0 +1,45 @@
+# for i in range(3,-1,-1):
+#     print('*'*(4-i)+' '*(i))
+
+
+# с помощью вложеных циклов
+# for i in range(1,5):
+#     for j in range((4-i),-1,-1):
+#         print('*'*i+' '*j)
+#         break
+
+# n=int(input())
+# s=False
+# for i in range(2,n):
+#     if n%i==0:
+#         s=True
+#         break
+# if not(s):
+#     print('составное')
+
+
+# a=int(input())
+# b=False
+# for i in range(a):
+#     if i%3==0 and i<a:
+#         b=True
+#         print(i)
+# if not(b):
+#     print('такий чисел нет')
+
+# a=input()
+# #in - в условиях проверяет принадлежность
+# b='aieouyAEOUYI'
+# for i in a:
+#     if i not in b:
+#         print(i,end='')#end - устанавливаем свой символ в конце по умолчанию \n
+
+
+# for i in range(3,-1,-1):
+#      print('*'*(4-i)+' '*(i))
+# for i in range(0,4):
+#      print('*'*(3-i)+' '*(i))
+
+# for i in range(0,6,5):
+#       print('+'*(5-i)+' '*(i),'+',sep='')
+
